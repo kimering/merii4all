fbff5bc9dede925693649cf464cdb04ed9a86d4a
kimering <merii029@naver.com>
2020-04-13 00:40:44 +0900

    create springboot project
    just created

---
+++ src/main/java/io/honeymon/boot/springboot/BootSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.honeymon.boot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSpringBootApplication.class, args);
+	}
+
+}
